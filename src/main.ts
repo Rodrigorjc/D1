185d6317fa5eb623adde9f47cd1d3bba5840a25a
Rodrigo <rjaencobos@safareyes.es>
2024-09-22 21:55:54 +0200

    comienzo de header

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,14 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+const searchIcon = document.getElementById('search-icon');
+if (searchIcon) {
+  searchIcon.addEventListener('click', function() {
+    const searchContainer = document.querySelector('.search-container');
+    if (searchContainer) {
+      searchContainer.classList.toggle('show-search');
+    }
+  });
+}
